aaa805ddb9a42159da66b4675e0c6b35acc18bb6
Al-Mamun Salauddin <almamunsalauddin1234@gmail.com>
2026-06-16 13:42:12 +0600

    conditional

---
+++ conditional.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a:=10
+
+	if a > 18 {
+		fmt.Println("You are eligible to vote")
+	} else {
+		fmt.Println("You are not eligible to vote")
+	}
+
+}
